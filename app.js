059b51a905315c243eafdcee37d846506b9d42fc
sugimotoy96 <sugimotoy96@yuna.local>
2020-10-01 14:04:28 +0900

    add header for  CORS

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.get('/', (req, res) => res.send('Hello World-GET'));
-app.post('/', (req, res) => res.send('Hello World-POST¥n'));
+app.post('/', (req, res) => res.send('{"items": ["tako","neko"] }'));
 
 app.listen(3002);
