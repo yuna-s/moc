df3a240946857a85acd3177f7537f3d7b31c51d9
杉本優奈 <yuna@YunasPC.local>
2020-10-27 23:24:28 +0900

    コメント付加

--- app.js
+++ app.js
@@ -28,11 +28,13 @@ app.get("/502/", (req, res) => res.status(502).send('後続APIエラー'));
 app.get("/504/", (req, res) => res.status(504).send('後続APIタイムアウト'));
 app.get("/timeout/", (req, res, next) => {
   (async () => {
-      await sleep(10*1000);
+    // 60秒
+      await sleep(60*1000);
       res.send('Timeout!!');
   })().catch(next);;
 });
 
+// post
 app.post("/", (req, res) => res.send('This is moc server!'));
 app.post("/400/", (req, res) => res.status(400).send('サーバーリクエストが無効'));
 app.post("/401/", (req, res) => res.status(401).send('未認証（未ログイン）'));
@@ -43,7 +45,8 @@ app.post("/502/", (req, res) => res.status(502).send('後続APIエラー'));
 app.post("/504/", (req, res) => res.status(504).send('後続APIタイムアウト'));
 app.post("/timeout/", (req, res, next) => {
   (async () => {
-      await sleep(10*1000);
+    // 60秒
+      await sleep(60*1000);
       res.send('Timeout!!');
   })().catch(next);;
 });
